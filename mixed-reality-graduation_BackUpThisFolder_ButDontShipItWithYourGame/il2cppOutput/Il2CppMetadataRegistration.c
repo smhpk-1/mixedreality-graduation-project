d03c98f547589529088bd772a040b289880574d7
Semiha <smhpk-1@users.noreply.github.com>
2025-12-11 00:20:45 +0300

    Major Build Update

---
+++ mixed-reality-graduation_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	12877,
+	g_Il2CppGenericTypes,
+	9050,
+	g_Il2CppGenericInstTable,
+	120103,
+	g_Il2CppGenericMethodFunctions,
+	37193,
+	g_Il2CppTypeTable,
+	138583,
+	g_Il2CppMethodSpecTable,
+	10025,
+	g_FieldOffsetTable,
+	10025,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
